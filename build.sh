f150e4b7c6144158c7d87b90ed913a8cc83bede7
Mike Hewson <hewsmike@iinet.net.au>
2020-04-28 13:28:06 +1000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+###########################################################################
+#   Copyright (C) 2020 by Mike Hewson                                     #
+#   hewsmike[AT]iinet.net.au                                              #
+#                                                                         #
+#   This is free software: you can redistribute it and/or modify          #
+#   it under the terms of the GNU General Public License as published     #
+#   by the Free Software Foundation, version 2 of the License.            #
+#                                                                         #
+#   BMP2RAW is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+#   GNU General Public License for more details.                          #
+#                                                                         #
+#   A copy of the GNU General Public License is available at              #
+#   http://www.gnu.org/licenses/                                          #
+#                                                                         #
+###########################################################################
+
+ROOT=`pwd`
